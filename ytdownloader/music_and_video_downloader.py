3215ddcfd262b0654a1fff5b518623c87592f6b4
GentianRrafshi <grrafshi@hotmail.de>
2019-07-01 04:15:58 +0200

    Better_GUI

--- ytdownloader/music_and_video_downloader.py
+++ ytdownloader/music_and_video_downloader.py
@@ -1,6 +1,47 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Tk, Label, Button
 import youtube_dl
+
+
+class DownloaderGUI:
+    def __init__(self, master):
+
+        self.Entries = []
+
+        self.master = master
+        master.title("Musik/Videodownloader")
+
+        self.label = Label(master, text="Wie viele Dateien willst du runterladen?")
+        self.label.grid(row=0, columnspan=2)
+
+        self.number = tk.Entry(master, bd=5, width=5)
+        self.number.grid(row=0, column=2, padx=5, pady=5)
+
+        self.greet_button = Button(master, text="OK", command=self.greet)
+        self.greet_button.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        #        b2 = tk.Button(root, text="Download MP4", command=(lambda e=ents: downloadVid(e)))
+        #        b2.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.download_mp3 = tk.Button(root, text="Download MP3", command=(lambda: downloadMP3(self.Entries)))
+        self.download_mp3.grid(row=(100 + 4), column=0, padx=5, pady=5, sticky=tk.W + tk.E)
+
+        self.download_vid = tk.Button(root, text="Download MP4", command=(lambda: downloadVid(self.Entries)))
+        self.download_vid.grid(row=(100 + 4), column=1, padx=5, pady=5, sticky=tk.W + tk.E)
+
+    def greet(self):
+        global i
+        w = tk.Label(root, text="Youtube Downloader")
+        w.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+        for i in range(int(self.number.get())):
+            Entry = tk.Entry(root, bd=5, width=30)
+            Entry.grid(row=(i + 3), column=0, padx=5, pady=5, columnspan=2)
+            self.Entries.append((i, Entry))
+        return self.Entries
 
 
 def downloadMP3(Entries):
@@ -38,32 +79,7 @@ def downloadVid(Entries):
             ydl.download([string])
 
 
-
-def GUI():
-    # Tinker simple GUI
-
-    w = tk.Label(root, text="Youtube Downloader")
-    w.pack()
-
-    Entries = []
-    for i in range(int(input('Wie viele '))):
-        Entry = tk.Entry(root, bd=5, width=50)
-        Entry.pack(side=tk.TOP)
-        Entries.append((i, Entry))
-    return Entries
-
-
 if __name__ == '__main__':
     root = tk.Tk()
-    ents = GUI()
-
-    b1 = tk.Button(root, text="Download MP3", command=(lambda e=ents: downloadMP3(e)))
-    b1.pack(side=tk.LEFT, padx=5, pady=5)
-
-    b2 = tk.Button(root, text="Download MP4", command=(lambda e=ents: downloadVid(e)))
-    b2.pack(side=tk.LEFT, padx=5, pady=5)
-
-    b3 = tk.Button(root, text='Quit', command=root.quit)
-    b3.pack(side=tk.LEFT, padx=5, pady=5)
-
+    my_gui = DownloaderGUI(root)
     root.mainloop()
