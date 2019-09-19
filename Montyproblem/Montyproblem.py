71037a072974d527791e4f4d7521acf45c8d731d
GentianRrafshi <grrafshi@hotmail.de>
2019-09-19 23:10:08 +0200

    Final abstract Version

--- Montyproblem/Montyproblem.py
+++ Montyproblem/Montyproblem.py
@@ -2,31 +2,28 @@
 
 """
 
-import argparse, random
+import random
+import matplotlib.pyplot as plt
 
-def simulate(num_doors, switch, verbose):
+
+def simulate(num_doors, switch):
     """(int, bool): bool
 
     Carry out the game for one contestant.  If 'switch' is True,
     the contestant will switch their chosen door when offered the chance.
     Returns a Boolean value telling whether the simulated contestant won.
     """
-
     # Doors are numbered from 0 up to num_doors-1 (inclusive).
-
     # Randomly choose the door hiding the prize.
     winning_door = random.randint(0, num_doors-1)
-    if verbose:
-        print('Prize is behind door {}'.format(winning_door+1))
-
     # The contestant picks a random door, too.
     choice = random.randint(0, num_doors-1)
-    if verbose:
-        print('Contestant chooses door {}'.format(choice+1))
-
     # The host opens all but two doors.
     closed_doors = list(range(num_doors))
-    while len(closed_doors) > 2:
+
+    open_doors = 3
+
+    while len(closed_doors) > open_doors:
         # Randomly choose a door to open.
         door_to_remove = random.choice(closed_doors)
 
@@ -37,17 +34,12 @@ def simulate(num_doors, switch, verbose):
 
         # Remove the door from the list of closed doors.
         closed_doors.remove(door_to_remove)
-        if verbose:
-            print('Host opens door {}'.format(door_to_remove+1))
 
     # There are always two doors remaining.
-    assert len(closed_doors) == 2
+    assert len(closed_doors) == open_doors
 
     # Does the contestant want to switch their choice?
     if switch:
-        if verbose:
-            print('Contestant switches from door {} '.format(choice+1), end='')
-
         # There are two closed doors left.  The contestant will never
         # choose the same door, so we'll remove that door as a choice.
         available_doors = list(closed_doors) # Make a copy of the list.
@@ -55,54 +47,52 @@ def simulate(num_doors, switch, verbose):
 
         # Change choice to the only door available.
         choice = available_doors.pop()
-        if verbose:
-            print('to {}'.format(choice+1))
 
     # Did the contestant win?
     won = (choice == winning_door)
-    if verbose:
-        if won:
-            print('Contestant WON', end='\n\n')
-        else:
-            print('Contestant LOST', end='\n\n')
     return won
 
 
 def main():
-    # Get command-line arguments
-    parser = argparse.ArgumentParser(
-        description='simulate the Monty Hall problem')
-    parser.add_argument('--doors', default=3, type=int, metavar='int',
-                        help='number of doors offered to the contestant')
-    parser.add_argument('--trials', default=100000, type=int, metavar='int',
-                        help='number of trials to perform')
-    parser.add_argument('--verbose', default=False, action='store_true',
-                        help='display the results of each trial')
-    args = parser.parse_args()
-
-    print('Simulating {} trials...'.format(args.trials))
-
     # Carry out the trials
+    x_axe = []
+    y_axe_switch = []
+    y_axe_stay = []
     winning_non_switchers = 0
     winning_switchers = 0
-    for i in range(args.trials):
+    counter = 0
+    num_doors = 4
+    trials = 100000
+
+    print('Simulating {} trials...'.format(trials))
+    for i in range(trials):
+        counter += 1
         # First, do a trial where the contestant never switches.
-        won = simulate(args.doors, switch=False, verbose=args.verbose)
+        won = simulate(num_doors, switch=False)
         if won:
             winning_non_switchers += 1
+        y_axe_stay.append(winning_non_switchers / counter)
 
         # Next, try one where the contestant switches.
-        won = simulate(args.doors, switch=True, verbose=args.verbose)
+        won = simulate(num_doors, switch=True)
         if won:
             winning_switchers += 1
+        y_axe_switch.append(winning_switchers / counter)
+        x_axe.append(counter)
 
     print('    Switching won {0:5} times out of {1} ({2}% of the time)'.format(
-            winning_switchers, args.trials,
-            (winning_switchers / args.trials * 100 ) ))
+            winning_switchers, 100,
+            (winning_switchers / trials * 100 ) ))
     print('Not switching won {0:5} times out of {1} ({2}% of the time)'.format(
-            winning_non_switchers, args.trials,
-            (winning_non_switchers / args.trials * 100 ) ))
+            winning_non_switchers, 100,
+            (winning_non_switchers / trials * 100 ) ))
 
+
+
+    plt.plot(x_axe, y_axe_switch, y_axe_stay)
+    plt.ylabel('Erfolgswahrscheinlichkeit')
+    plt.xlabel('Anzahl Simulationen')
+    plt.show()
 
 if __name__ == '__main__':
     main()
