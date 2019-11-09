f1160d97f4568de518c6443fd13ff949981e3869
Gentian <grrafshi@hotmail.de>
2019-11-09 17:01:44 +0100

    Lorem Ipsum

--- Wurfelspiel/Wurfelspiel_Punktzahl.py
+++ Wurfelspiel/Wurfelspiel_Punktzahl.py
@@ -14,19 +14,16 @@ def dice(number_of_points):
             k = False
             if die == 1:
                 sum = 0
-#                print('Number of throws to get "1":', counter)
         result.append(die)
 
     result = np.asarray(result)
     summe = np.sum(sum)
-#    print(result)
-#    print(summe)
     return summe
 
 
 if __name__ == '__main__':
     number_of_points = 20  # Number of minimum points
-    number_of_simulation = 1000000  # Number of Simulations
+    number_of_simulation = 100  # Number of Simulations
     sim = np.array([dice(number_of_points) for i in range(number_of_simulation)])
     number_of_losses = collections.Counter(sim)[0]
     average = np.sum(sim)/number_of_simulation
@@ -53,10 +50,10 @@ if __name__ == '__main__':
                   size=25,
                   weight='bold')
         plt.tick_params(labelsize=20)
-#        plt.xticks(np.arange(0, number_of_throws*6 + 1, 1))
-#        plt.ylim(0, number_of_losses*1.25)
+        plt.ylim(0, number_of_losses * 1.1)
+        plt.xlim(0, number_of_points + 7)
 
-        plt.xlabel('Ausgewählter Kandidat', fontsize=25, fontweight='bold')
+        plt.xlabel('Gesamtpunktzahl', fontsize=25, fontweight='bold')
         plt.ylabel('Häufigkeit', fontsize=25, fontweight='bold')
     plt.grid(True)
     plt.show()
